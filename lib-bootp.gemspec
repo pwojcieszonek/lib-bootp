e03e4cd59357601189af34e27b546c3f2e5ecd4b
Piotr Wojcieszonek <piotr.wojcieszonek@tkk.net.pl>
2018-01-08 14:00:17 +0100

    Change IPAddr library to Net::Address::IPv4

--- lib-bootp.gemspec
+++ lib-bootp.gemspec
@@ -24,5 +24,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
 
+  spec.add_dependency 'net-address', '~> 0.1'
+
   spec.required_ruby_version = '>= 2.2.0'
 end
--- lib/lib/bootp/packet/ip_address.rb
+++ lib/lib/bootp/packet/ip_address.rb
@@ -3,36 +3,16 @@
 # Copyright 18.03.2016 by Piotr Wojcieszonek
 
 require 'forwardable'
-require 'ipaddr'
-#require 'comparable'
+require 'net/address'
+
 
 module Lib
   module BOOTP
     class Packet
-      class IPAddress
-        extend Forwardable
-        include Comparable
-
-        def_delegators :@ip, :to_s, :to_i
-
-        def initialize(ip)
-          @ip = if ip.is_a?(IPAddr)
-                  ip
-                else
-                  raise ArgumentError, "Wrong IP Address #{ip}" if ip.nil? || ip == ''
-                  IPAddr.new(ip, Socket::AF_INET)
-                end
-          raise ArgumentError, "Wrong IP Address #{ip}" unless @ip.is_a?(IPAddr)
-        end
-
-        def <=>(other)
-          other = IPAddr.new(other, Socket::AF_INET) unless other.is_a?(IPAddr)
-          self.to_i <=> other.to_i
-        end
+      class IPAddress < ::Net::Address::IPv4
 
         def pack
-          @ip = 0 if @ip.nil?
-          [@ip.to_i].pack('N')
+          [self.to_i].pack('N')
         end
 
         def self.unpack(ip)
